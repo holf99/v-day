b693724c8cf817c39477248fb0c533a478ac0acd
holf99 <36529771+holf99@users.noreply.github.com>
2026-03-23 01:09:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    "happy.jpg",    // 0 normal
+    "please.jpg",  // 1 confused
+    "really.jpg",             // 2 pleading
+    "moreSad.jpg",             // 3 sad
+    "sad.jpg",       // 4 sadder
+    "crazy.jpg",             // 5 devastated
+    "please.jpg",               // 6 very devastated
+    "please.jpg"  // 7 crying runaway
 ]
 
 const noMessages = [
     "No",
-    "Are you positive? 🤔",
-    "Pookie please... 🥺",
-    "If you say no, I will be really sad...",
+    "Wrong button babe, try again",
+    "HAHA not funny.. 🥺",
+    "Come on Habibti!",
     "I will be very sad... 😢",
-    "Please??? 💔",
+    "Nothing you can do Muahahaha",
     "Don't do this to me...",
     "Last chance! 😭",
     "You can't catch me anyway 😜"
